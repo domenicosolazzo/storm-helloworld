5280c3f5694409ff88b238ea2c9b5aa2e07f659b
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-12-20 12:13:46 +0100

    Implemented the method for the spout

--- src/main/java/storm/cookbook/HelloWorldSpout.java
+++ src/main/java/storm/cookbook/HelloWorldSpout.java
@@ -1,11 +1,49 @@
 package storm.cookbook;
 
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.Map;
+import java.util.Random;
+
 /**
- * Created with IntelliJ IDEA.
  * User: domenicosolazzo
- * Date: 12/20/13
- * Time: 12:06 PM
- * To change this template use File | Settings | File Templates.
  */
-public class HelloWorldSpout {
+public class HelloWorldSpout extends BaseRichSpout {
+
+    private SpoutOutputCollector collector;
+    private int referenceRandom;
+    private static final int MAX_RANDOM = 10;
+    public HelloWorldSpout(){
+        final Random rand = new Random();
+        referenceRandom = rand.nextInt( MAX_RANDOM );
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+        outputFieldsDeclarer.declare(new Fields("sentence"));
+    }
+
+    @Override
+    public void open(Map map, TopologyContext topologyContext, SpoutOutputCollector spoutOutputCollector) {
+        this.collector = collector;
+    }
+
+    @Override
+    public void nextTuple() {
+        Utils.sleep(100);
+        final Random rand = new Random();
+        int instanceRandom = rand.nextInt(MAX_RANDOM);
+        if(instanceRandom == referenceRandom){
+            collector.emit(new Values("Hello World"));
+        } else {
+            collector.emit(new Values("Other Random Word"));
+
+        }
+    }
 }
