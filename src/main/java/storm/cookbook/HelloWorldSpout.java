916d0522e0cf9d460a1039813ad0481ff9b461d7
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-12-20 12:08:16 +0100

    Added HelloWorldSpout

---
+++ src/main/java/storm/cookbook/HelloWorldSpout.java
@@ -0,0 +1,11 @@
+package storm.cookbook;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: domenicosolazzo
+ * Date: 12/20/13
+ * Time: 12:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldSpout {
+}
